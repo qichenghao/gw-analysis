a1a09deb1aab3860a020db9edd233ec72e02f905
qichenghao <qch940504@163.com>
2019-09-24 15:03:56 +0800

    测试分支

---
+++ src/main/java/com/qtone/gy/utils/TestUtil.java
@@ -0,0 +1,20 @@
+package com.qtone.gy.utils;/**
+ * @ClassName TestUtil.java
+ * @author qichenghao
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2019年09月24日 15:02:00
+ */
+
+/**
+ * 　　* @className: TestUtil
+ * 　　* @author qichenghao
+ * 　　* @date 2019/9/24 15:02
+ * <p>
+ *
+ */
+public class TestUtil {
+    public static void main(String[] args){
+        System.out.println("测试git");
+    }
+}
